eac2fe31499605b756e8a5e03fdb25baf9ce076e
sai143cool <saigangadhar143@gmail.com>
2024-01-13 01:17:46 +0530

    started Javascript

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+console.log("Hello World");  //it will print the output to browser console
+
+//This is comment
+
+/* using Let keyword for practise */
+
+let MyName = "Sai Gangadhar Reddy"; //assigning MyName variable with 'let' key word
+console.log(MyName.length); //it will return length of characters which is 19
+console.log(5 + MyName.length); //it will add the value 5 and length of the string which is 19
+
+
+function sum(x, y) {  //syntax for the function
+    return x + y;  //return is the keyword to end of the function
+    console.log("Hello World"); // this will NEVER run
+}
+
+console.log('Hey'.startsWith('H')); //it will return boolean value
+
+console.log('    Remove whitespace   '.trim()); // it will remove empty space or white space
